b16e62c6dfaeb78235271d026ffc192dfb99e7a7
Marcus Jensen <marcusjensen1515@gmail.com>
2020-04-11 13:01:22 -0700

    correclty adjusting message depending on the navigated page

--- index.js
+++ index.js
@@ -5,12 +5,25 @@
 //createServer is a built-in method of the http package, accepts a function as a parameter.
 //function provided to createServer is a callback function, its called when createServer function is compleated
 //When this callback function is called, it will be provided the request (req) and a response (res) object in the function. we can do whatever we want with these requests and responses in the body of the function.
-//server.listen is telling it which local host to run this app on. 
+//server.listen is telling it which local host to run this app on.
+//This console log will appear in the terminal, not in the chrome console.
 
 const http = require('http');
 const server = http.createServer((req,res) => {
-  console.log(req.url);
-  res.end('Hello Node.js');
+
+  if(req.url === '/about'){
+    res.end('The about page');
+  }
+  else if (req.url === '/contact'){
+    res.end('The contact page');
+  }
+  else if (req.url === '/'){
+    res.end('The home page');
+  }
+  else {
+    res.writeHead(404);
+    res.end('page not found');
+  }
 });
 
 server.listen(3000);
