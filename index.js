1dcdf04d74df1951a10110d2d28936e3bc775fc1
Marcus Jensen <marcusjensen1515@gmail.com>
2020-04-11 12:51:12 -0700

    initial hello world print

---
+++ index.js
@@ -0,0 +1,16 @@
+//require is the same as import or include in other languages
+//you can use http as you would any other object
+//it is good practice to have modules and variables having the same name (like http in this example)
+//http is a built in package that lets us perform actions on the createServer
+//createServer is a built-in method of the http package, accepts a function as a parameter.
+//function provided to createServer is a callback function, its called when createServer function is compleated
+//When this callback function is called, it will be provided the request (req) and a response (res) object in the function. we can do whatever we want with these requests and responses in the body of the function.
+//server.listen is telling it which local host to run this app on. 
+
+const http = require('http');
+const server = http.createServer((req,res) => {
+  console.log(req.url);
+  res.end('Hello Node.js');
+});
+
+server.listen(3000);
