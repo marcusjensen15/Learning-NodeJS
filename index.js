aaeb6027580f3e68661a6f294277d23da1f67872
Marcus Jensen <marcusjensen1515@gmail.com>
2020-04-11 13:46:54 -0700

    add express to app

--- index.js
+++ index.js
@@ -11,29 +11,65 @@
 // module fs is a file system module that helps us interact with files on the server.
 // readFileSync reads the content of each file and returns it. content is stored in variable.
 //this function recieves a request, and decides how to respond to it. Its a request handler. (req-request, res-response)
-//every Node.js app is like this. there is a single request handler that responds to everything. 
+//every Node.js app is like this. there is a single request handler that responds to everything.
 
-const http = require('http');
-const fs = require('fs');
-const homePage = fs.readFileSync('index.html');
-const aboutPage = fs.readFileSync('about.html');
-const contactPage = fs.readFileSync('contact.html');
-const notFoundPage = fs.readFileSync('notfound.html');
-const server = http.createServer((req,res) => {
+// const http = require('http');
+// const fs = require('fs');
+// const homePage = fs.readFileSync('index.html');
+// const aboutPage = fs.readFileSync('about.html');
+// const contactPage = fs.readFileSync('contact.html');
+// const notFoundPage = fs.readFileSync('notfound.html');
+// const server = http.createServer((req,res) => {
+//
+//   if(req.url === '/about'){
+//     res.end(aboutPage);
+//   }
+//   else if (req.url === '/contact'){
+//     res.end(contactPage);
+//   }
+//   else if (req.url === '/'){
+//     res.end(homePage);
+//   }
+//   else {
+//     res.writeHead(404);
+//     res.end(notFoundPage);
+//   }
+// });
+//
+// server.listen(3000);
 
-  if(req.url === '/about'){
-    res.end(aboutPage);
-  }
-  else if (req.url === '/contact'){
-    res.end(contactPage);
-  }
-  else if (req.url === '/'){
-    res.end(homePage);
-  }
-  else {
-    res.writeHead(404);
-    res.end(notFoundPage);
-  }
-});
 
-server.listen(3000);
+//express version below
+
+//path module lets us define a specific path. is the same regardless of OS
+//sendFile is a built in express method.
+
+const path = require('path');
+const express = require('express'); //requires express module
+const app = express() //calls express function to start new Express app
+app.listen(3000, () => {
+  console.log("app is listening on port 3000");
+})
+
+// app.get('/',(req,res) =>{
+//   res.json({
+//     name: 'Marcus Jensen'
+//   })
+// })
+
+//path example
+
+app.get('/',(req,res) =>{
+  res.sendFile(path.resolve(__dirname,'index.html'))
+})
+
+
+
+app.get('/about',(req,res) => {
+  res.sendFile(path.resolve(__dirname,'about.html'))
+})
+
+app.get('/contact', (req,res) => {
+
+  res.sendFile(path.resolve(__dirname,'contact.html'))
+})
